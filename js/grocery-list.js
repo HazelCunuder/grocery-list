aba2b5fcdbeddf4545f1ba53b15f32d1310713af
Hazel <basile.cunuder@gmail.com>
2025-05-21 09:18:58 +0200

    feat: add function to display placeholder message

--- js/grocery-list.js
+++ js/grocery-list.js
@@ -6,3 +6,13 @@ const emptyMessage = document.getElementById('empty-list');
 const inputField = document.getElementById('input');
 const shoppingList = document.getElementById('shopping-list');
 
+let groceryItems = [];
+renderList();
+
+function renderList() {
+  console.log("rendering List...");
+  groceryItems.length === 0 ? emptyMessage.style.display = "block"
+  : emptyMessage.style.display = "none";
+};
+
+
